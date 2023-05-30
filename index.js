d2ad1a0f73c1827e79e5d7677e24e60e10ed9f02
ChrisChrome <christophercookman@gmail.com>
2023-05-29 20:18:23 -0600

    Guh

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require("./config.json");
 const colors = require("colors");
+const axios = require("axios");
 
 // Setup Discord
 const Discord = require("discord.js");
@@ -34,15 +35,20 @@ const express = require("express");
 const app = express();
 
 // Setup Routes
-app.get("/send/:data", (req, res) => {
+app.get("/send/:data", async (req, res) => {
 	console.log(req.params.data);
 	const data = JSON.parse(decodeURIComponent(req.params.data));
 	if (data.password !== config.password) return res.sendStatus(403);
-	webhook.send({
-		isUser: true,
-		username: data.name,
-		content: data.msg
-	})
+	console.log(data.sid)
+	await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${config.steamkey}&steamids=${data.sid}`).then((response) => {
+		console.log(response.data)
+		webhook.send({
+			isUser: true,
+			username: data.name,
+			content: data.msg,
+			avatarURL: response.data.response.players[0].avatarfull
+		})
+	});
 	res.sendStatus(204);
 });
 
