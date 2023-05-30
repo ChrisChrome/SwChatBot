7d4b455bd80082ecba38ee5439a6ca6a9c4ac6b4
ChrisChrome <christophercookman@gmail.com>
2023-05-29 20:00:52 -0600

    Yuh

---
+++ index.js
@@ -0,0 +1,59 @@
+const config = require("./config.json");
+const colors = require("colors");
+
+// Setup Discord
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: ["GuildMessages", "MessageContent", "Guilds", "GuildMembers"]})
+
+// Setup Webhook
+const webhook = new Discord.WebhookClient({url: config.discord.webhook}, {allowedMentions: {parse: []}});
+
+// Setup Discord Stuff
+client.on("ready", () => {
+	console.log(`${colors.cyan("[INFO]")} Logged In As ${client.user.tag}!`);
+	// start express server
+	app.listen(config.port, () => {
+		console.log(`${colors.cyan("[INFO]")} Started Express Server On Port ${config.port}!`);
+		console.log(`${colors.cyan("[INFO]")} Startup Took ${new Date() - startTime}ms!`);
+	});
+});
+
+client.on("messageCreate", async (message) => {
+	if (message.author.bot) return;
+	if (message.channel.type === "DM") return;
+	if (message.channel.id !== config.discord.channel) return;
+	if (message.content.length == 0) return;
+	msgBuffer.push({
+		name: message.author.tag,
+		msg: message.content,
+	});
+});
+
+// Setup Express
+const express = require("express");
+const app = express();
+
+// Setup Routes
+app.get("/send/:data", (req, res) => {
+	console.log(req.params.data);
+	const data = JSON.parse(decodeURIComponent(req.params.data));
+	if (data.password !== config.password) return res.sendStatus(403);
+	webhook.send({
+		isUser: true,
+		username: data.name,
+		content: data.msg
+	})
+	res.sendStatus(204);
+});
+
+app.get("/get/:password", (req, res) => {
+	if (req.params.password !== config.password) return res.sendStatus(403);
+	res.send(msgBuffer);
+	msgBuffer = [];
+});
+
+var msgBuffer = [];
+
+const startTime = new Date();
+console.log(`${colors.cyan("[INFO]")} Starting Up...`);
+client.login(config.discord.token)
